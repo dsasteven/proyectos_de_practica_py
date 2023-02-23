07c8d03416dba0e0b615492b295b6881b1957315
dsasteven <dsasd306@gmail.com>
2023-02-23 17:07:58 -0500

    puse pa guardar las imagenes

--- app/charts.py
+++ app/charts.py
@@ -4,14 +4,15 @@ def generate_bar_chart(labels, values ):
 
     fig, ax = plt.subplots()
     ax.bar(labels, values)
-    plt.show()
-    
+    plt.savefig('bar.png')
+    plt.close()
 def generate_pie_chart(labels, values ):
 
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis('equal')
-    plt.show()
+    plt.savefig('pie.png')
+    plt.close()
     
 
 if __name__ == "__main__":

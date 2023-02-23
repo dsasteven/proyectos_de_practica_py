31277761f1a08125d0d529481222d6c45ba2410d
dsasteven <dsasd306@gmail.com>
2023-02-23 13:55:19 -0500

    agrege la carpeta app

---
+++ app/utils.py
@@ -0,0 +1,29 @@
+def get_population(country_dict):
+  population_dict = {
+    '2022': int(country_dict['2022 Population']),
+    '2020': int(country_dict['2020 Population']),
+    '2015': int(country_dict['2015 Population']),
+    '2010': int(country_dict['2010 Population']),
+    '2000': int(country_dict['2000 Population']),
+    '1990': int(country_dict['1990 Population']),
+    '1980': int(country_dict['1980 Population']),
+    '1970': int(country_dict['1970 Population'])
+  }
+  labels = population_dict.keys()
+  values = population_dict.values()
+  return labels, values
+
+
+
+def country_percentage(data):
+  poputation_percentage_dict = ({item['Country/Territory']: item['World Population Percentage'] for item in data})
+  
+  labels = poputation_percentage_dict.keys()
+  values = poputation_percentage_dict.values()
+
+  print(poputation_percentage_dict)
+  return labels, values
+
+def population_by_country(data, country):
+  result = list(filter(lambda item: item['Country/Territory'] == country, data))
+  return result
